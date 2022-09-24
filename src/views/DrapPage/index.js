03dd91ff9c9707f8cf4cb2ae587dc3438f6b88fc
PhamPhu19498 <ppphu2912@gmail.com>
2022-09-24 11:20:10 +0700

    update link external

--- src/views/DrapPage/index.js
+++ src/views/DrapPage/index.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Flex } from "@phamphu19498/livetrade-uikit";
+import { Flex, Text, LinkExternal } from "@phamphu19498/livetrade-uikit";
 import DragList from "./DragList";
 
 const DrapPages = () =>{
     return (
         <Flex width="100%" justifyContent="center" alignItems="center" flexDirection="column" py="2rem">
-            <Flex maxWidth="1320px" width='100%' justifyContent='center' pt="2rem">
+            <Flex maxWidth="1320px" width='100%' justifyContent='center' pt="2rem" flexDirection="column">
+                <Flex width="100%" justifyContent='flex-start' mb="2rem">
+                    <LinkExternal 
+                        href="https://github.com/PhamPhu19498/demo-drap-app"
+                        style={{cursor:"pointer"}}
+                    >
+                        <Text bold>
+                            Link GitHub
+                        </Text>
+                    </LinkExternal>
+                </Flex>
                 <DragList/>
             </Flex>
         </Flex>
